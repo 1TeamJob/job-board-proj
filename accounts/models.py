c925cbe6a28f3b7bc008da8160dae3b160d10bf8
1TeamJob <i.team.job77@gmail.com>
2022-05-01 22:39:15 +0300

    adding profile and editing profile pages with abillty to edit the profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.ForeignKey('address', on_delete=models.CASCADE, blank=True, null=True)
+    phone_number = models.CharField(max_length=13)
+    profile_photo = models.ImageField(upload_to='profile/')
+    created_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return str(self.user)
+    
+    
+# when Craete New User --> create user profile manually.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    
+
+class Address(models.Model):
+    address = models.CharField(max_length=50)
+    
+    
+    def __str__(self):
+        return self.address
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
+from django.urls import reverse
+from .forms import SignupForm, UserForm, ProfileForm
 from django.contrib.auth import authenticate, login
+from .models import Profile
+
 
 def signup(request):
     if request.method == 'POST':
@@ -19,3 +22,32 @@ def signup(request):
     
     context = {'form': form}
     return render(request, 'registration/signup.html', context)
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)    
+    context = {'profile': profile}
+    return render(request, 'accounts/profile.html', context)
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, request.Files)
+        profile_form = ProfileForm(request.POST, request.Files)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            any_profile = profile_form.save(commit=False)
+            any_profile.user = request.user
+            any_profile.save()
+            return redirect(reverse('accounts:profile'))
+    
+    
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=profile)
+        
+    context = {}
+    return render(request, 'accounts/edit_profile.html', context)
+
+
+
